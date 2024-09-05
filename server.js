6eab0dc77f38b5e163cc3ba7be441423a6e91d0a
Luis Mineo <luisafonsomineo@gmail.com>
2024-09-05 09:23:06 -0300

    adicionado README e pequeno polimento

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ app.use(express.json());
 app.options('*', cors());
 app.use('/api', routes);
 
-app.use(cors({
-    origin: 'http://localhost:5500/',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
