6dcab6538daa1813095a40e9a560eb61a609809b
Luis Mineo <luisafonsomineo@gmail.com>
2024-09-05 09:01:25 -0300

    resolvido temp cors/adicionado o frontend

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.options('*', cors());
 app.use('/api', routes);
+
+app.use(cors({
+    origin: 'http://localhost:5500/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
